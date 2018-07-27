aaf244ea128c22f26ac3f2f781a811cd89286723
Sergheev Andrian <sergheev@icloud.com>
2018-07-27 08:10:23 +0300

    Initial Commit

---
+++ MobileConfigProfileInstall/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MobileConfigProfileInstall
+//
+//  Created by Andrian Sergheev on 7/27/18.
+//  Copyright © 2018 Andrian Sergheev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
